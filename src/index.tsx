f540cf7a53764f7b45060a812c17e16c9b588a0c
dudueasy <dudueasy@126.com>
2019-04-19 01:01:27 +0800

    更新入口: 分别引入 Button 组件和 ButtonReact 组件

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,39 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import Button from './Button';
+import ButtonReact from './ButtonReact';
 import './index.css';
 
 
 ReactDOM.render(
-  <Fragment>
-    <div >Hello World
-      <div><Button size="small"/> small button</div>
-      <div><Button size=""/> normal button</div>
-      <div><Button size="big" /> big button </div>
-    </div>
-  </Fragment>
-  ,
-  document.getElementById('root')
+    <Fragment>
+        <div >Hello World
+            <div>
+                <Button size="small">small</Button>
+            </div>
+            <div>
+                <Button >normal</Button>
+            </div>
+            <div>
+                <Button size="big">big</Button>
+            </div>
+
+            <div>
+                <Button size="big">
+                    <span>这个按钮接收了一个 span 元素</span>
+                </Button>
+            </div>
+
+
+
+
+            <div>
+                <ButtonReact size="big">
+                    <span>这是一个 ButtonReact</span>
+                </ButtonReact>
+            </div>
+        </div>
+    </Fragment>
+    ,
+    document.getElementById('root')
 );
